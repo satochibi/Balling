1dcbabc5ccabac2a0623955e1b5775fb7ce3bf5b
satochibi <satochibi0120@gmail.com>
2017-07-12 14:27:34 +0900

    球に力を加えてストライクをとるまで

---
+++ Assets/BallController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float thrust;
+
+    // Use this for initialization
+    void Start () {
+        this.GetComponent<Rigidbody>().AddForce(transform.forward * thrust);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
